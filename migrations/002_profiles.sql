a36955fd7ba237ed128f0431af7aac3edab183a6
Ali Mengilli <dowjones579@gmail.com>
2025-09-08 19:01:33 +0300

    update 002_profiles.sql

--- migrations/002_profiles.sql
+++ migrations/002_profiles.sql
@@ -1,6 +1,8 @@
 -- migrations/002_profiles.sql
-CREATE TABLE IF NOT EXISTS profiles (
-    player_id INTEGER PRIMARY KEY REFERENCES players(id) ON DELETE CASCADE,
+DROP TABLE IF EXISTS profiles CASCADE;
+
+CREATE TABLE profiles (
+    player_id INTEGER PRIMARY KEY,
     data JSONB NOT NULL DEFAULT '{}'::jsonb,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
